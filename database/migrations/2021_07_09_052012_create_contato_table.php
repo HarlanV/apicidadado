6368357fab14388e4edae3b953951b0b48f0a1b1
HarlanV <harlan.eq@gmail.com>
2021-07-09 02:51:37 -0300

    Migration de criacao da tabela contato. Novo padrado em PT

---
+++ database/migrations/2021_07_09_052012_create_contato_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContatoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contato', function (Blueprint $table) {
+            $table->id();
+            $table->string('tipo')->comment('Deve indicar se é email ou telefone (email//tel)');
+            $table->string('contato')->comment('Preencher com o email ou telefone do cidadao');
+            $table->foreignId('cidadao_id')
+                ->references('id')
+                ->on('cidadao');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contato');
+    }
+}
