af3e98ef045bece73517010c0115783c6078be31
HarlanV <harlan.eq@gmail.com>
2021-07-09 02:53:57 -0300

    Criacao da model Endereco

---
+++ app/Models/Endereco.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Cidadao;
+
+class Endereco extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'cep',
+        'logradouro',
+        'complemento',
+        'bairro',
+        'cidade',
+        'estado',
+        'cidade'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+
+    public function cidadao()
+    {
+        return $this->belongsTo(Cidadao::class);
+    }
+}
