835ffa4a0bcca6619cd217889d5a22c0fa030400
HarlanV <harlan.eq@gmail.com>
2021-07-09 02:51:57 -0300

    Migration de criacao da tabela endereco. Novo padrado em PT

---
+++ database/migrations/2021_07_09_052033_create_endereco_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEnderecoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('endereco', function (Blueprint $table) {
+            $table->id();
+            $table->string('cep');
+            $table->string('logradouro');
+            $table->string('complemento')->nullable();
+            $table->string('bairro');
+            $table->string('cidade');
+            $table->string('estado');
+            $table->timestamps();
+            $table->foreignId('cidadao_id')
+                ->references('id')
+                ->on('cidadao');
+            $table->softDeletes();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('endereco');
+    }
+}
